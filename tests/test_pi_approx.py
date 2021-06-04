6b20fb3379b804b8b5386bf4d9715e92c0f34d70
Bernát Gábor <gaborjbernat@gmail.com>
2021-06-04 22:36:50 +0100

    Keep C extension only
    Signed-off-by: Bernát Gábor <gaborjbernat@gmail.com>

--- tests/test_pi_approx.py
+++ tests/test_pi_approx.py
@@ -1,11 +1,6 @@
 from math import pi
 
-from pi_approx import __version__, approximate_pi
-
-
-def test_version() -> None:
-    assert __version__
-
+from pi_approx_cy import approximate_pi
 
 def test_approx() -> None:
     assert (approximate_pi(300) - pi) < 0.1
